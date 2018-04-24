d16c78c0f8740253ef3f5e9fc23e8613172bf7ee
Robert Shaw <robertshaw87@gmail.com>
2018-04-24 00:16:40 -0700

    create initial files, add psuedocode to game.js, make default html to be fleshed out later

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// declare initial variable values
+// make an array of the alphabet
+// make an empty array of the letters already guessed by the user
+// decide on a random letter to be the target letter
+
+// get user guess
+// clean user input
+
+// check user input to make sure it is a new guess
+
+// if it is the correct letter, 
+// add 1 to wins, reset guesses and guess array to default
+// set a new random letter to be guessed
+
+// if it is not the correct letter,
+// add guess to guess array, reduce guesses left
+// go back to getting user input
